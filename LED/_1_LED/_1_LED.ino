60e913e8a9c6f45a92c32ce7f56338c64ba43d77
znx <elba.oretti@gmail.com>
2022-10-12 22:42:38 +0200

    1led

--- LED/_1_LED/_1_LED.ino
+++ LED/_1_LED/_1_LED.ino
@@ -1,19 +1,20 @@
 /*
-    17 ottobre 2021 - accensione LED via software
-    
-    modificato il 7 marzo 2022
+   12 ottobre 2022 Marchi Antonino
+   
+   STA 2C e 2F - accensione LED via software 
 
 */
 
-#define LED_VERDE 8  // Associo al LED_VERDE il numero 8
-
+int LED_VERDE = 8;  // Associo al LED_VERDE il numero 8
 int timeBlink = 1000;  // definisco un tempo di lampeggiamento
 
+/* Impostazioni iniziali del programma */
 void setup()
 {
     pinMode(LED_VERDE, OUTPUT); // definisco la porta come output
 }
 
+/* Ciclo del programma da eseguire */
 void loop()
 {
     digitalWrite(LED_VERDE, LOW); // spengo il led
